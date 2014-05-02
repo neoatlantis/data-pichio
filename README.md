4950fe995abefbc0cee4b487af098d68e09f4c85
NeoAtlantis <rijndael@yeah.net>
2014-05-02 20:18:08 +0800

    note a sample private key

--- README.md
+++ README.md
@@ -18,6 +18,15 @@ a briefcase, in which the Data-Pichio collects such photos and stores them
 encrypted in your laptop. Even if you are found, no data in your computer is
 able to be proved being the photos.
 
+Sample Key
+----------
+
+The sample key sent to this repository have a corresponding private key of
+following. **DO NOT make your own private key public, and DO NOT use this
+out-of-box private key!! You have been warned!**
+
+  9d3c66ab edb914e7 4e453c12 7b7320d7 5cd78d63 69e26b36 0f824605 8e3ef52c 
+
 ------------------------------------------------------------------------------
 
 数据貔貅
@@ -34,4 +43,10 @@ Wifi-SD卡是启动后能产生一个Wifi热点，然后允许通过无线LAN访
 在笔记本电脑上，数据貔貅收集热点上的照片并加密存储。即使之后您被发现，也没什么
 可以证明这些加密的文件就是照片。
 
+示例密钥
+--------
 
+公布在本库配置文件中的公钥对应的私钥如下。**您不能公开自己的私钥！也不要使用这
+个自带的私钥！勿谓吾言之不预！**
+
+  9d3c66ab edb914e7 4e453c12 7b7320d7 5cd78d63 69e26b36 0f824605 8e3ef52c 
--- pichio.js
+++ pichio.js
@@ -1,2 +1,55 @@
+#!/usr/local/bin/node
+
 require('./lib');
+
+var publicKeyPath = $.tools.resolve('conf', 'public-key'),
+    algorithm = $.config["encryption"]["algorithm"];
+
+if(!$.node.fs.existsSync(publicKeyPath)){
+    var privateKey = $.crypto.random.bytes(32);
+    var deriver = $.crypto.acrypt(algorithm);
+    deriver.setPrivateKey(privateKey);
+
+    var publicKey = deriver.getPublicKey();
+
+    var privateKeyHex = privateKey.toString('hex').toLowerCase(),
+        publicKeyB64 = publicKey.toString('base64'),
+        privateKeyShow = '',
+        publicKeyShow = '';
+
+    for(var i=0; i<privateKeyHex.length; i+=8)
+        privateKeyShow += privateKeyHex.slice(i, i+8) + ' ';
+
+    for(var i=0; i<publicKeyB64.length; i+=64)
+        publicKeyShow += '  ' + publicKeyB64.slice(i, i+64) + '\n';
+
+    console.log(
+        "\nWelcome to use Data-Pichio!\n" +
+        "---------------------------\n" +
+        "You have not yet specified your public key.\n" +
+        "System have randomly choosen one for you.\n\n" +
+
+        "Your PRIVATE key is following, please write it down carefully\n" +
+        "and DOUBLE CHECK its correctness!.\n\n  " +
+        privateKeyShow + '\n\n' +
+        "Your Public Key is following, please save it to the config file.\n" +
+        '\n' +
+        publicKeyShow + '\n' +
+        'The config for public key is usually in [./conf/public-key].\n' +
+        'Create this file, if it doesn\'t exists.\n\n' +
+        'Your note of PRIVATE key is useful for recovering swallowed data\n' +
+        'in this system. But it is no use, to bring this note together\n' +
+        'and leaving the unfriendly people clues. Secure it! You will NOT\n' +
+        'need it during data capturing activities!!\n'
+    );
+    process.exit(0);
+};
+
+// read public key
+var publicKey = $.node.fs.readFileSync($.tools.resolve('conf', 'public-key'));
+publicKey = publicKey.toString('ascii').replace(/[^0-9a-z\/\+=]/gi, '');
+publicKey = new $.node.buffer.Buffer(publicKey, 'base64');
+
+var encryptor = $.crypto.acrypt(algorithm);
+encryptor.setPublicKey(publicKey);
 
