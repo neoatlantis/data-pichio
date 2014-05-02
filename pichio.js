de8da0db1ef9184b438f37f248e430ff9c42c63c
NeoAtlantis <rijndael@yeah.net>
2014-05-03 02:44:46 +0800

    add server

--- pichio.js
+++ pichio.js
@@ -93,9 +93,15 @@ function save(srcName, metaJSON, dataBuffer, CB){
     $.node.async.waterfall(workflow, CB);
 };
 
+/*
 save(
     'default',
     {'a':'a'},
     $.node.fs.readFileSync('sample'),
     console.log
-);
+);*/
+
+/////////////////////////////// HTTP SERVER //////////////////////////////////
+if($.config.server){
+    $.server(storagePath, $.config.server);
+};
